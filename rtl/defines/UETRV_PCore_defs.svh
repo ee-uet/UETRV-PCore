b6430322177b936e286d76aac7601df61724ea66
Abd <abdulwadood.afzal88@gmail.com>
2022-04-12 14:54:22 +0500

    Added rtl for decode and regfile modules

---
+++ rtl/defines/UETRV_PCore_defs.svh
@@ -0,0 +1,15 @@
+`ifndef UETRV_PCORE_SVH
+`define UETRV_PCORE_SVH
+
+
+//------------------------------------------------------------------------------
+// CORE PARAMETERS
+//------------------------------------------------------------------------------
+
+// Width of main registers and buses
+`define XLEN               32
+
+`define RF_AWIDTH           5
+`define RF_SIZE            32
+
+`endif // UETRV_PCORE_SVH
