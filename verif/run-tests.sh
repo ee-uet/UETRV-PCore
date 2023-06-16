12fafb18a0520829e1734d2e91a7545af9540462
Ali Imran <59641896+ALI11-2000@users.noreply.github.com>
2023-06-16 19:00:40 +0500

    Create run-tests.sh

---
+++ verif/run-tests.sh
@@ -0,0 +1,31 @@
+# !bin/bash
+
+set -e
+
+riscof -v debug run --config=config.ini \
+           --suite=riscv-arch-test/riscv-test-suite/rv32i_m/I \
+           --env=riscv-arch-test/riscv-test-suite/env \
+           --no-browser
+
+if grep -rniq riscof_work/report.html -e '>0failed<'
+then
+    echo "Test successful!"
+    exit 0
+else
+    echo "Test FAILED!"
+    exit 1
+fi
+
+riscof -v debug run --config=config.ini \
+           --suite=riscv-arch-test/riscv-test-suite//rv32i_m/M \
+           --env=riscv-arch-test/riscv-test-suite/env \
+           --no-browser
+
+if grep -rniq riscof_work/report.html -e '>0failed<'
+then
+    echo "Test successful!"
+    exit 0
+else
+    echo "Test FAILED!"
+    exit 1
+fi
