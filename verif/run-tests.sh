9784a14f5157f2b8d4f008633c6a3f7e31ad77b5
Abdul Wadood <abdulwadood.afzal88@gmail.com>
2023-06-26 20:24:13 +0500

    Added script to run arch-tests
    Signed-off-by: Abdul Wadood <abdulwadood.afzal88@gmail.com>

--- verif/run-tests.sh
+++ verif/run-tests.sh
@@ -4,29 +4,15 @@ set -e
 error=0
 
 riscof -v debug run --config=config.ini \
-           --suite=riscv-arch-test/riscv-test-suite/rv32i_m/I \
+           --suite=riscv-arch-test/riscv-test-suite/ \
            --env=riscv-arch-test/riscv-test-suite/env \
            --no-browser
 
 if grep -rniq riscof_work/report.html -e '>0failed<'
 then
     echo "Test successful!"
-    error=$((error+0))
+    exit 0
 else
     echo "Test FAILED!"
-    error=$((error+1))
-fi
-
-riscof -v debug run --config=config.ini \
-           --suite=riscv-arch-test/riscv-test-suite//rv32i_m/M \
-           --env=riscv-arch-test/riscv-test-suite/env \
-           --no-browser
-
-if grep -rniq riscof_work/report.html -e '>0failed<'
-then
-    echo "Test successful!"
-    error=$((error+0))
-else
-    echo "Test FAILED!"
-    error=$((error+1))
+    exit 1
 fi
