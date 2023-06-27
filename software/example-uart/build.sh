fd105f28bae3456fa7e32164daf040726df2b7c0
Muhammad Tahir <mtahir@uet.edu.pk>
2023-06-27 10:09:27 +0500

    Update repo

--- software/example-uart/build.sh
+++ software/example-uart/build.sh
@@ -7,12 +7,10 @@ riscv64-unknown-elf-gcc -c -O -o build/uart.o Interfaces/uart.c -march=rv32im_zi
 riscv64-unknown-elf-gcc -c -O -o build/plic.o Interfaces/plic.c -march=rv32im_zicsr -mabi=ilp32
 riscv64-unknown-elf-gcc -c -O -o build/main.o src/main.c -march=rv32im_zicsr -mabi=ilp32
 
-riscv64-unknown-elf-gcc -O -o build/program.elf build/startup.o build/isr.o build/uart.o build/plic.o build/main.o -T linker.ld -nostdlib -march=rv32i_zicsr -mabi=ilp32
+riscv64-unknown-elf-gcc -O -o build/main.elf build/startup.o build/isr.o build/uart.o build/plic.o build/main.o -T linker.ld -nostdlib -march=rv32i_zicsr -mabi=ilp32
 
-riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/program.elf build/main.bin
-
-hexdump build/main.bin > build/main.hex
+riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/main.elf build/main.bin
 
 python3 maketxt.py build/main.bin > build/hello.txt
 
-riscv64-unknown-elf-objdump -S -s build/program.elf > build/program.dump
+riscv64-unknown-elf-objdump -S -s build/main.elf > build/main.dump
--- software/example-uart/make.bat
+++ software/example-uart/make.bat
@@ -7,17 +7,12 @@ riscv64-unknown-elf-gcc -c -O -o build/uart.o Interfaces/uart.c -march=rv32im -m
 riscv64-unknown-elf-gcc -c -O -o build/plic.o Interfaces/plic.c -march=rv32im -mabi=ilp32
 riscv64-unknown-elf-gcc -c -O -o build/main.o src/main.c -march=rv32im -mabi=ilp32
 
-riscv64-unknown-elf-gcc -O -o build/program.elf build/startup.o build/isr.o build/uart.o build/plic.o build/main.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-gcc -O -o build/main.elf build/startup.o build/isr.o build/uart.o build/plic.o build/main.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
 
-riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/program.elf build/main.bin
+riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/main.elf build/main.bin
 
-hexdump build/main.bin > build/main.hex
+python3 maketxt.py build/main.bin > build/hello.txt
 
-python maketxt.py build/main.bin > build/imem.txt
+riscv64-unknown-elf-objdump -S -s build/main.elf > build/main.dump
 
-riscv64-unknown-elf-objdump -S -s build/program.elf > build/program.dump
-
-REM riscv64-unknown-elf-objcopy --input-target=binary --output-target=elf32-little build/bbl.bin build/bbl.elf
-
-REM riscv64-unknown-elf-objdump -S -s build/bbl.elf > build/bbl.dump
 
