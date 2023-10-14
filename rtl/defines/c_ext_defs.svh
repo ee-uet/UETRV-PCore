997947f461b1ff69fbf0fccd89839da6d6762a4a
Ateeb Tahir <ateeb.tahir.16@gmail.com>
2023-10-15 00:04:59 +0500

    Create c_ext_defs.svh

---
+++ rtl/defines/c_ext_defs.svh
@@ -0,0 +1,10 @@
+// Copyright 2023 University of Engineering and Technology Lahore.
+// Licensed under the Apache License, Version 2.0, see LICENSE file for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Description:  
+//
+// Author: Ateeb Tahir, DDRC, UET Lahore
+// Date: 21.7.2023
+
+
